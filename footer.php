3d41df88a9fc76855d889b2667ed85328f564199
Jonathan Holroyd <jonny.holroyd@gmail.com>
2016-12-05 12:00:10 +0000

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-    <!-- javascript -->
-    <script src="assets/js/jquery-2.1.1.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="libraries/bootstrap-select/js/bootstrap-select.min.js"></script>
-    <script src="assets/js/sb-admin-2.js"></script>
-    
-</body>
-</html>
