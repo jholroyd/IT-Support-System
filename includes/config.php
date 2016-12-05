4fcfae7a55734bdb23698ee92daf006bb77d23b6
Jonathan Holroyd <jonny.holroyd@gmail.com>
2016-12-05 11:58:21 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,20 @@
+<?php
 
+session_start();
+
+$config = array(
+    'host'		=> 'localhost',			// define the database host.
+    'username'	=> 'root',				// define the database user.
+    'password'	=> '',			      // define the database password.
+    'dbname'	=> 'support'		    // define the database name.
+);
+
+$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+include('functions.php');
+include('classes/bcrypt.php');
+
+$bcrypt = new bcrypt();
+
+?>
